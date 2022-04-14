4eda5677dea65035bbf80bd9be4cac78fb460f9e
Bubbliiiing <3323290568@qq.com>
2022-04-14 17:04:59 +0800

    update multi gpus

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -12,100 +12,102 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, optimizer, epoch,
 
     if local_rank == 0:
         print('Start Train')
+        pbar = tqdm(total=epoch_step,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
     model_train.train()
-    with tqdm(total=epoch_step,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
-        for iteration, batch in enumerate(gen):
-            if iteration >= epoch_step:
-                break
+    for iteration, batch in enumerate(gen):
+        if iteration >= epoch_step:
+            break
 
-            images, targets = batch[0], batch[1]
-            with torch.no_grad():
-                if cuda:
-                    images  = torch.from_numpy(images).type(torch.FloatTensor).cuda()
-                    targets = [torch.from_numpy(ann).type(torch.FloatTensor).cuda() for ann in targets]
-                else:
-                    images  = torch.from_numpy(images).type(torch.FloatTensor)
-                    targets = [torch.from_numpy(ann).type(torch.FloatTensor) for ann in targets]
+        images, targets = batch[0], batch[1]
+        with torch.no_grad():
+            if cuda:
+                images  = torch.from_numpy(images).type(torch.FloatTensor).cuda()
+                targets = [torch.from_numpy(ann).type(torch.FloatTensor).cuda() for ann in targets]
+            else:
+                images  = torch.from_numpy(images).type(torch.FloatTensor)
+                targets = [torch.from_numpy(ann).type(torch.FloatTensor) for ann in targets]
+        #----------------------#
+        #   清零梯度
+        #----------------------#
+        optimizer.zero_grad()
+        if not fp16:
+            #----------------------#
+            #   前向传播
+            #----------------------#
+            outputs         = model_train(images)
+
+            #----------------------#
+            #   计算损失
+            #----------------------#
+            loss_value = yolo_loss(outputs, targets)
+
+            #----------------------#
+            #   反向传播
+            #----------------------#
+            loss_value.backward()
+            optimizer.step()
+        else:
+            from torch.cuda.amp import autocast
+            with autocast():
+                outputs = model_train(images)
+                #----------------------#
+                #   计算损失
+                #----------------------#
+                loss_value = yolo_loss(outputs, targets)
+
+            #----------------------#
+            #   反向传播
+            #----------------------#
+            scaler.scale(loss_value).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+        loss += loss_value.item()
+        
+        if local_rank == 0:
+            pbar.set_postfix(**{'loss'  : loss / (iteration + 1), 
+                                'lr'    : get_lr(optimizer)})
+            pbar.update(1)
+
+    if local_rank == 0:
+        pbar.close()
+        print('Finish Train')
+        print('Start Validation')
+        pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
+
+    model_train.eval()
+    for iteration, batch in enumerate(gen_val):
+        if iteration >= epoch_step_val:
+            break
+        images, targets = batch[0], batch[1]
+        with torch.no_grad():
+            if cuda:
+                images  = torch.from_numpy(images).type(torch.FloatTensor).cuda()
+                targets = [torch.from_numpy(ann).type(torch.FloatTensor).cuda() for ann in targets]
+            else:
+                images  = torch.from_numpy(images).type(torch.FloatTensor)
+                targets = [torch.from_numpy(ann).type(torch.FloatTensor) for ann in targets]
             #----------------------#
             #   清零梯度
             #----------------------#
             optimizer.zero_grad()
-            if not fp16:
-                #----------------------#
-                #   前向传播
-                #----------------------#
-                outputs         = model_train(images)
+            #----------------------#
+            #   前向传播
+            #----------------------#
+            outputs         = model_train(images)
 
-                #----------------------#
-                #   计算损失
-                #----------------------#
-                loss_value = yolo_loss(outputs, targets)
+            #----------------------#
+            #   计算损失
+            #----------------------#
+            loss_value = yolo_loss(outputs, targets)
 
-                #----------------------#
-                #   反向传播
-                #----------------------#
-                loss_value.backward()
-                optimizer.step()
-            else:
-                from torch.cuda.amp import autocast
-                with autocast():
-                    outputs = model_train(images)
-                    #----------------------#
-                    #   计算损失
-                    #----------------------#
-                    loss_value = yolo_loss(outputs, targets)
-
-                #----------------------#
-                #   反向传播
-                #----------------------#
-                scaler.scale(loss_value).backward()
-                scaler.step(optimizer)
-                scaler.update()
-
-            loss += loss_value.item()
-            
-            if local_rank == 0:
-                pbar.set_postfix(**{'loss'  : loss / (iteration + 1), 
-                                    'lr'    : get_lr(optimizer)})
-                pbar.update(1)
+        val_loss += loss_value.item()
+        if local_rank == 0:
+            pbar.set_postfix(**{'val_loss': val_loss / (iteration + 1)})
+            pbar.update(1)
 
     if local_rank == 0:
-        print('Finish Train')
-        print('Start Validation')
-
-    model_train.eval()
-    with tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
-        for iteration, batch in enumerate(gen_val):
-            if iteration >= epoch_step_val:
-                break
-            images, targets = batch[0], batch[1]
-            with torch.no_grad():
-                if cuda:
-                    images  = torch.from_numpy(images).type(torch.FloatTensor).cuda()
-                    targets = [torch.from_numpy(ann).type(torch.FloatTensor).cuda() for ann in targets]
-                else:
-                    images  = torch.from_numpy(images).type(torch.FloatTensor)
-                    targets = [torch.from_numpy(ann).type(torch.FloatTensor) for ann in targets]
-                #----------------------#
-                #   清零梯度
-                #----------------------#
-                optimizer.zero_grad()
-                #----------------------#
-                #   前向传播
-                #----------------------#
-                outputs         = model_train(images)
-
-                #----------------------#
-                #   计算损失
-                #----------------------#
-                loss_value = yolo_loss(outputs, targets)
-
-            val_loss += loss_value.item()
-            if local_rank == 0:
-                pbar.set_postfix(**{'val_loss': val_loss / (iteration + 1)})
-                pbar.update(1)
-
-    if local_rank == 0:
+        pbar.close()
         print('Finish Validation')
         loss_history.append_loss(epoch + 1, loss / epoch_step, val_loss / epoch_step_val)
         print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
