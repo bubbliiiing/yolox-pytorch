af7550e8a1af478f3ba8cf071878900a44ab9c5c
Bubbliiiing <3323290568@qq.com>
2022-05-01 13:21:49 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,13 +2,30 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 import torch
+from thop import clever_format, profile
 from torchsummary import summary
 
 from nets.yolo import YoloBody
 
 if __name__ == "__main__":
+    input_shape = [640, 640]
+    num_classes = 80
+    phi         = 'l'
+    
     # 需要使用device来指定网络在GPU还是CPU运行
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    m       = YoloBody(80, 's').to(device)
+    m       = YoloBody(num_classes, phi).to(device)
+    summary(m, (3, input_shape[0], input_shape[1]))
     
-    summary(m, input_size=(3, 640, 640))
+    dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
+    flops, params   = profile(m.to(device), (dummy_input, ), verbose=False)
+    #--------------------------------------------------------#
+    #   flops * 2是因为profile没有将卷积作为两个operations
+    #   有些论文将卷积算乘法、加法两个operations。此时乘2
+    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
+    #   本代码选择乘2，参考YOLOX。
+    #--------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.3f")
+    print('Total GFLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
